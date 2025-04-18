717129863725a3b9f5cac3292ca510845ff01e50
Benjamin Sebastian <142651700+SebastianBenjamin@users.noreply.github.com>
2025-04-18 14:47:14 +0530

    forwarding for logging modals

--- src/main/java/org/servlets/fetchServlet.java
+++ src/main/java/org/servlets/fetchServlet.java
@@ -1,4 +1,28 @@
-//package org.servlets;
-//
-//public class fetchServlet extends htto{
-//}
+package org.servlets;
+
+import org.classFiles.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/index")
+public class fetchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        User user1=session.get(User.class, 1);
+
+        req.setAttribute("user", user1);
+        req.getRequestDispatcher("/dashboard.jsp").forward(req, resp);
+    }
+}
